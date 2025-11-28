de2ccfbc54075d278cb67638de8e034ca537d0ca
CambridgeXR <chris.macdonald@lucy.cam.ac.uk>
2025-11-28 15:23:11 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,45 @@
-const CACHE_NAME = 'vr-explorer-v1';
-const OFFLINE_URLS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+const CACHE_NAME = 'vr-explorer-v2.03';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
+  // Force this service worker to become the active one immediately
+  self.skipWaiting(); 
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS))
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  // Network-first strategy:
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+  const cacheWhitelist = [CACHE_NAME];
+  // Take control of all pages immediately
+  event.waitUntil(
+    Promise.all([
+      self.clients.claim(),
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    ])
   );
 });
